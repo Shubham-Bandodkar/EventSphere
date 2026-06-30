720c4db328abd078f098e9548600684f600e6946
ShubhamBandodkar <shubham25bandodkar@gmail.com>
2026-06-30 09:31:00 +0530

    Phase 0: Initial Angular setup with Material and Tailwind

---
+++ src/app/layouts/home-layout/home-layout.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-layout',
+  imports: [],
+  templateUrl: './home-layout.html',
+  styleUrl: './home-layout.css',
+})
+export class HomeLayout {}
